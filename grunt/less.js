3fd8340b24bca7cb617cd12cff22490f8adc7b17
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2016-02-17 19:28:48 +0200

    Walk thtough LESS till mixins

--- grunt/less.js
+++ grunt/less.js
@@ -2,8 +2,7 @@ module.exports = {
   all: {
     files: {
       'less-way/css/base.css': 'less-way/less/base.less',
-      'less-way/css/app.css': 'less-way/less/app.less',
-      'less-way/css/buttons.css': 'less-way/less/buttons.less'
+      'less-way/css/app.css': 'less-way/less/app.less'
     }
   }
 };
